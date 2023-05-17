d43b3fd44199694959d11a4e0869dd3db74549d5
Liel Huri <liel.huri@post.runi.ac.il>
2023-05-17 14:17:49 +0300

    Implemented Board.cs

--- EX02/B23 Ex02 Yali 206910333 Liel 209207380/Ex02/Board.cs
+++ EX02/B23 Ex02 Yali 206910333 Liel 209207380/Ex02/Board.cs
@@ -10,24 +10,47 @@ namespace Ex02
     {
         private Players[,] m_BoardMatrix;
 
-        public Board()
+        public Board(int i_BoardSize)
         {
-
+            m_BoardMatrix = new Players[i_BoardSize, i_BoardSize];
         }
 
-        internal Players getPlayerAt(Tuple<int,int> i_Coordinate)
+        internal Players getPlayerAt((int, int) i_Coordinate)
         {
-            return Players.NONE;
+            return m_BoardMatrix[i_Coordinate.Item1, i_Coordinate.Item2];
         }
 
-        internal void setPlayerAt(Tuple<int,int> i_Coordinate, Player i_Player)
+        internal void setPlayerAt((int,int) i_Coordinate, Players i_Player)
         {
-
+            if (isCellEmpty(i_Coordinate))
+            {
+                m_BoardMatrix[i_Coordinate.Item1, i_Coordinate.Item2] = i_Player;
+            }
+            else
+            {
+                throw new AccessViolationException();
+            }
         }
 
+        private bool isCellEmpty((int,int) i_Coordinate)
+        {
+            return m_BoardMatrix[i_Coordinate.Item1, i_Coordinate.Item2] == Players.NONE;
+        }
+
+        /**
+         * Check indentation inside for loops
+         * Check for Matrix length attributes
+         * Check another method implementations
+         */
         internal void ResetMatrix()
         {
-
+            for(int i=0; i<m_BoardMatrix.Length; i++)
+            {
+                for(int j=0; j<m_BoardMatrix.Length; j++)
+                {
+                    m_BoardMatrix[i, j] = Players.NONE;
+                }
+            }
         }
     }
 }
