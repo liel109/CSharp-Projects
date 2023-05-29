71bae8775508d9f7a1fd83e6330bf96a6652a021
Yali Gal <yaligal1212@gmail.com>
2023-05-29 19:13:23 +0300

    VehicleFactory implemantation

--- EX03/B23 Ex03 Yali 206910333 Liel 209207380/Ex03.GarageLogic/VehicleFactory.cs
+++ EX03/B23 Ex03 Yali 206910333 Liel 209207380/Ex03.GarageLogic/VehicleFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
 
 namespace Ex03.GarageLogic
 {
@@ -38,9 +39,40 @@ namespace Ex03.GarageLogic
             { eVehicleType.Truck, 135f}
         };
 
-        public static Vehicle CreateNewVehicle(eVehicleType i_VehicleType)
+        public static Vehicle CreateNewVehicle(string i_LicensePlate, eVehicleType i_VehicleType)
         {
-            return null;
+            Vehicle vehicle;
+            Engine engine = createEngine(i_VehicleType);
+            Vehicle.Wheel[] wheels = createWheels(i_VehicleType);
+
+            switch (i_VehicleType)
+            {
+                case eVehicleType.ElectricCar:
+                    vehicle = new ElectricCar(i_LicensePlate, engine as ElectricEngine, wheels);
+                    break;
+
+                case eVehicleType.PetrolCar:
+                    vehicle = new PetrolCar(i_LicensePlate, engine as PetrolEngine, wheels);
+                    break;
+
+                case eVehicleType.ElectricMotorcyle:
+                    vehicle = new ElecticMotorcycle(i_LicensePlate, engine as ElectricEngine, wheels);
+                    break;
+
+                case eVehicleType.PetrolMotorcycle:
+                    vehicle = new PetrolMotorcycle(i_LicensePlate, engine as PetrolEngine, wheels);
+                    break;
+
+                case eVehicleType.Truck:
+                    vehicle = new Truck(i_LicensePlate, engine as PetrolEngine, wheels);
+                    break;
+
+                default:
+                    vehicle = null;
+                    break;
+            }
+
+            return vehicle;
         }
 
         public enum eVehicleType 
@@ -51,6 +83,35 @@ namespace Ex03.GarageLogic
             PetrolMotorcycle,
             Truck
         }
+
+        private static Engine createEngine(eVehicleType i_VehicleType)
+        {
+            Engine engine = null;
+
+            if (i_VehicleType == eVehicleType.PetrolCar || i_VehicleType == eVehicleType.PetrolMotorcycle || i_VehicleType == eVehicleType.Truck)
+            {
+                engine = new PetrolEngine(sr_FuelTypeDict[i_VehicleType], sr_MaxEngineCapacity[i_VehicleType]);
+            }
+            else if(i_VehicleType == eVehicleType.ElectricCar || i_VehicleType == eVehicleType.ElectricMotorcyle)
+            {
+                engine = new ElectricEngine(sr_MaxEngineCapacity[i_VehicleType]);
+            }
+
+            return engine;
+        }
+
+        private static Vehicle.Wheel[] createWheels(eVehicleType i_VehicleType)
+        {
+            Vehicle.Wheel[] wheels = new Vehicle.Wheel[sr_NumOfWheelsDict[i_VehicleType]];
+            
+            for(int i=0; i<wheels.Length; i++)
+            {
+                wheels[i] = new Vehicle.Wheel(sr_MaxWheelAirPressureDict[i_VehicleType]);
+            }
+
+            return wheels;
+        } 
+
 
     }
 }
