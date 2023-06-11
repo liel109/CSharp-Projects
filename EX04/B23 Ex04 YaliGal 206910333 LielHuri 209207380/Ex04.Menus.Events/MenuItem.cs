48b339d0b479ff33f7f52d9724cb7ed1f136350f
Yali Gal <yaligal1212@gmail.com>
2023-06-11 23:41:05 +0300

    MenuItem implemantation

--- EX04/B23 Ex04 YaliGal 206910333 LielHuri 209207380/Ex04.Menus.Events/MenuItem.cs
+++ EX04/B23 Ex04 YaliGal 206910333 LielHuri 209207380/Ex04.Menus.Events/MenuItem.cs
@@ -6,7 +6,7 @@ namespace Ex04.Menus.Events
     public class MenuItem
     {
         private string m_Title;
-        private readonly List<MenuItem> r_SubMenuItems;
+        private readonly List<MenuItem> r_SubMenuItems = new List<MenuItem>();
         public event Action<MenuItem> Selected;
 
         public string Title
@@ -19,29 +19,51 @@ namespace Ex04.Menus.Events
             get { return r_SubMenuItems; }
         }
 
-        public MenuItem()
+        public MenuItem(string i_Title)
         {
-            
+            m_Title = i_Title;
         }
 
         public void AddSubMenuItem(MenuItem i_MenuItemToAdd)
         {
-
+            r_SubMenuItems.Add(i_MenuItemToAdd);
         }
 
         public void AddSubMenuItem(string i_NewSubMenuItemTitle)
         {
+            MenuItem itemToAdd = new MenuItem(i_NewSubMenuItemTitle);
+            AddSubMenuItem(itemToAdd);
+        }
+        public void RemoveSubMenuItem(MenuItem i_MenuItemToAdd)
+        {
+            r_SubMenuItems.Remove(i_MenuItemToAdd);
+        }
 
+        public void RemoveSubMenuItem(string i_NewSubMenuItemTitle)
+        {
+            MenuItem itemToAdd = new MenuItem(i_NewSubMenuItemTitle);
+            RemoveSubMenuItem(itemToAdd);
         }
 
         public MenuItem GetMenuItemByTitle(string i_RequestedMenuItemTitle)
         {
-            return null;
+            MenuItem itemToReturn = null;
+
+            foreach(MenuItem menuItem in r_SubMenuItems)
+            {
+                if (menuItem.Title == i_RequestedMenuItemTitle)
+                {
+                    itemToReturn = menuItem;
+                    break;
+                }
+            }
+
+            return itemToReturn;
         }
 
         internal void SelectedByUser()
         {
-
+            OnSelected();
         }
 
         protected virtual void OnSelected()
--- EX04/B23 Ex04 YaliGal 206910333 LielHuri 209207380/Ex04.Menus.Interfaces/MenuItem.cs
+++ EX04/B23 Ex04 YaliGal 206910333 LielHuri 209207380/Ex04.Menus.Interfaces/MenuItem.cs
@@ -6,7 +6,7 @@ namespace Ex04.Menus.Interfaces
     public class MenuItem
     {
         private string m_Title;
-        private readonly List<MenuItem> r_SubMenuItems;
+        private readonly List<MenuItem> r_SubMenuItems = new List<MenuItem>();
         private readonly List<ISelectedListener> r_SelectedListeners = new List<ISelectedListener>();
 
         public string Title
@@ -19,43 +19,69 @@ namespace Ex04.Menus.Interfaces
             get { return r_SubMenuItems; }
         }
 
-        public MenuItem()
+        public MenuItem(string i_Title)
         {
-            
+            m_Title= i_Title;
         }
 
         public void AddSelectedListener(ISelectedListener i_ListenerToAdd)
         {
-
+            r_SelectedListeners.Add(i_ListenerToAdd);
         }
 
         public void RemoveSelectedListener(ISelectedListener i_ListenerToRemove)
         {
-
+            r_SelectedListeners.Remove(i_ListenerToRemove);
         }
 
         public void AddSubMenuItem(MenuItem i_MenuItemToAdd)
         {
-
+            r_SubMenuItems.Add(i_MenuItemToAdd);
         }
 
         public void AddSubMenuItem(string i_NewSubMenuItemTitle)
         {
+            MenuItem itemToAdd = new MenuItem(i_NewSubMenuItemTitle);
+            AddSubMenuItem(itemToAdd);
+        }
+        public void RemoveSubMenuItem(MenuItem i_MenuItemToAdd)
+        {
+            r_SubMenuItems.Remove(i_MenuItemToAdd);
+        }
 
+        public void RemoveSubMenuItem(string i_NewSubMenuItemTitle)
+        {
+            MenuItem itemToAdd = new MenuItem(i_NewSubMenuItemTitle);
+            RemoveSubMenuItem(itemToAdd);
         }
 
         public MenuItem GetMenuItemByTitle(string i_RequestedMenuItemTitle)
         {
-            return null;
+            MenuItem itemToReturn = null;
+
+            foreach (MenuItem menuItem in r_SubMenuItems)
+            {
+                if (menuItem.Title == i_RequestedMenuItemTitle)
+                {
+                    itemToReturn = menuItem;
+                    break;
+                }
+            }
+
+            return itemToReturn;
         }
 
         internal void SelectedByUser()
         {
-
+            OnSelected();
         }
 
         private void OnSelected()
         {
+            foreach(ISelectedListener listener in r_SelectedListeners)
+            {
+                listener.NotifySelected(this);
+            } 
         }
     }
 }
